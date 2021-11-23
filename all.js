bf0919a8cc7476c1e3559ab98ac5ecab5ca55ce8
Karen Huang <thinknfix2021@gmail.com>
2021-11-23 23:33:57 +0800

    判斷式的問題

--- all.js
+++ all.js
@@ -1,41 +1,20 @@
-let people = [
-    {
-        area: "高雄",
-        name: "小明"
-    },
-    {
-        area: "高雄",
-        name: "小天"
-    },
-    {
-        area: "台中",
-        name: "小華"
-    },
-    {
-        area: "台北",
-        name: "小華"
-    }
-]
+let localData = []
 
-// 覆蓋原本 people 陣列內的資料   
 axios.get('https://raw.githubusercontent.com/hexschool/js-training/main/travelAPI-lv1.json')
     .then(function (response) {
-        people = response.data;
-        // console.log(people)
-        render();
-        // drawChart();
+        localData = response.data;
+        // console.log(localData)
+        drawDiagram();
     });
 
 const ticketCardArea = document.querySelector('.ticketCard-area');
 
-function render() {
+const render = () => {
 
-    let eachCardInfo = ''
+    let ticketCardContent = '';
 
-    people.forEach(function (item) {
-        // console.log(item.area)
-        eachCardInfo += `
-            <li class="ticketCard">
+    localData.forEach(function (item) {
+        ticketCardContent += `<li class="ticketCard">
                 <div class="ticketCard-img">
                   <a href="#">
                     <img src="${item.imgUrl}" alt="">
@@ -49,7 +28,7 @@ function render() {
                       <a href="#" class="ticketCard-name">${item.name}</a>
                     </h3>
                     <p class="ticketCard-description">
-                        ${item.description}
+                      ${item.description}
                     </p>
                   </div>
                   <div class="ticketCard-info">
@@ -63,32 +42,33 @@ function render() {
                   </div>
                 </div>
               </li>`
+        ticketCardArea.innerHTML = ticketCardContent;
+
+        const searchResult = document.querySelector('#searchResult-text');
+        let result = `本次搜尋共 ${localData.length} 筆資料`;
+        searchResult.textContent = result;
+
     })
-
-    ticketCardArea.innerHTML = eachCardInfo;
-
-    // 套票筆數
-    const ticketAmount = document.getElementById('searchResult-text');
-    ticketAmount.textContent = `本次搜尋共 ${people.length} 筆資料`
-
-    drawChart();
 }
 
-//  新增套票按鈕上 綁定監聽事件 
+// 增加票卡按鈕
 const addTicketBtn = document.querySelector('.addTicket-btn');
-addTicketBtn.addEventListener('click', addCard)
 
-const ticketName = document.getElementById('ticketName');
-const ticketImgUrl = document.getElementById('ticketImgUrl');
-const ticketRegion = document.getElementById('ticketRegion');
-const ticketPrice = document.getElementById('ticketPrice');
-const ticketNum = document.getElementById('ticketNum');
-const ticketRate = document.getElementById('ticketRate');
-const ticketDescription = document.getElementById('ticketDescription');
+// 整個表單的 div
+const wholeForm = document.querySelector('.addTicket-form');
 
-function addCard() {
+// 表單內各個欄位
+const ticketName = document.querySelector('#ticketName');
+const ticketImgUrl = document.querySelector('#ticketImgUrl');
+const ticketRegion = document.querySelector('#ticketRegion');
+const ticketPrice = document.querySelector('#ticketPrice');
+const ticketNum = document.querySelector('#ticketNum');
+const ticketRate = document.querySelector('#ticketRate');
+const ticketDescription = document.querySelector('#ticketDescription');
 
-    people.push({
+const addCard = () => {
+
+    localData.push({
         "id": Date.now(),
         "name": ticketName.value,
         "imgUrl": ticketImgUrl.value,
@@ -99,75 +79,59 @@ function addCard() {
         "description": ticketDescription.value
     })
 
-    const wholeTicketForm = document.querySelector('.addTicket-form');
-    wholeTicketForm.reset();
-
-    render();
-    // drawChart();
+    wholeForm.reset();
+    drawDiagram();
+    // render();
 
 }
+addTicketBtn.addEventListener('click', addCard);
 
 
+function drawDiagram() {
 
-// 篩選地區，並累加數字上去
-// totalObj 會變成 {高雄: 1, 台北: 1, 台中: 1}
+    let totalAreaObject = {}
 
-let totalObj = {};
+    localData.forEach(function (item) {
 
-function getTotalObject() {
+        if (totalAreaObject[item['area']] === undefined) {
 
-    people.forEach(function (item, index) {
-        console.log(item)
-        if (totalObj[item.area] == undefined) {
-            totalObj[item.area] = 1;
+            totalAreaObject[item['area']] = 1;
+
+        // } else if (totalAreaObject[item['area']] === item['area']) {
+            
         } else {
-            totalObj[item.area] += 1;
+
+            totalAreaObject[item['area']] += 1;
         }
+
+    })
+    console.log(totalAreaObject)
+
+    const newAreaArray = Object.keys(totalAreaObject);  // ['台北', '台中', '高雄']
+
+    let newData = []
+    newAreaArray.forEach(function (item) {
+        let newObj = []
+        newObj.push(item);
+        newObj.push(totalAreaObject[item]);
+        newData.push(newObj);
     })
 
-}
+    console.log(newData)
 
-// newData = [["高雄", 1], ["台北",1], ["台中", 1]]
-let newData = [];
-
-function getNewData() {
-    getTotalObject();
-    // 宣告 area 變數，用來儲存 Object.keys(totalObj) 的值
-    let area = Object.keys(totalObj);
-
-    // area output ["高雄","台北","台中"]
-    area.forEach(function (item, index) {
-        let ary = [];
-        ary.push(item);
-        ary.push(totalObj[item]);
-        newData.push(ary);
-    })
-
-    // totalObj = {}
-
-}
-
-function drawChart() {
-
-    getNewData();
-    // 將 newData 丟入 c3 產生器
-    const chart = c3.generate({
-        bindto: "#chart",
+    // donut 圖
+    var chart = c3.generate({
+        bindto: '#areaDonut',
         data: {
             columns: newData,
             type: 'donut',
-            colors: {
-                "高雄": '#E68619',
-                "台北": '#26BFC7',
-                "台中": '#5151D3'
-            },
         },
         donut: {
             title: "套票地區比重"
-        },
-
+        }
     });
 
-    // newData = []
-
+    render();
 }
+
+
